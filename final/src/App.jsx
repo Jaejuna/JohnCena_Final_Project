d786e0e1fdee597240d2fefb74ad6b36b403ccd8
Jaejuna <anayana9988@gmail.com>
2020-12-18 21:59:07 +0900

    created customer jsx

--- final/src/App.jsx
+++ final/src/App.jsx
@@ -1,11 +1,49 @@
 import React, {Component} from 'react';
 import './App.css';
+import Customer from './components/Customer';
+
+const customer = [{
+  'id' : '1',
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '이철수',
+  'birthday' : '951208',
+  'gender' : '남자',
+  'job' : '대학생',
+},
+{
+  'id' : '2',
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '정짱구',
+  'birthday' : '990809',
+  'gender' : '남자',
+  'job' : '개발자',
+},
+{
+  'id' : '3',
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '김맹구',
+  'birthday' : '990424',
+  'gender' : '남자',
+  'job' : '디자이너',
+}
+]
 
 class App extends Component{
   render(){
   return (
-    <div className="gray-background">
-      <h2>Final project for open source devlopment basic lecture</h2>
+    <div>
+      { customer.map(c => {
+          return (
+            <Customer
+            key = {c.id}
+            id = {c.id}
+            image = {c.image}
+            name = {c.name}
+            birthday = {c.birthday}
+            gender = {c.gender}
+            job = {c.job}
+            />
+          )})}
     </div>
   );}}
 
