ed79d2dedad9005d332033e1dc4f41195f0e33d7
Jaejuna <anayana9988@gmail.com>
2020-12-18 22:31:18 +0900

    material UI added

--- final/src/App.jsx
+++ final/src/App.jsx
@@ -1,6 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
+import Paper from '@material-ui/core/Paper';
 import Customer from './components/Customer';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
 
 const customer = [{
   'id' : '1',
@@ -30,9 +48,22 @@ const customer = [{
 
 class App extends Component{
   render(){
+  const {classes} = this.props;
   return (
-    <div>
-      { customer.map(c => {
+    <Paper ClassName = {classes.root}>
+      <Table ClassName = {classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        { customer.map(c => {
           return (
             <Customer
             key = {c.id}
@@ -44,7 +75,9 @@ class App extends Component{
             job = {c.job}
             />
           )})}
-    </div>
+        </TableBody>
+      </Table>
+    </Paper>
   );}}
 
-export default App;
+export default withStyles(styles)(App);
