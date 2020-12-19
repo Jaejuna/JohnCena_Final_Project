69018bb3fa728e205fbfbf7ab49959eec827975b
lym0659 <youngmin0659@gmail.com>
2020-12-19 17:13:24 +0900

    delete function create

--- final/server.js
+++ final/server.js
@@ -25,6 +25,7 @@ const upload = multer({dest: './upload'});
 
 app.get('/api/customers', (req,res) => {
   connection.query(
+    "SELECT * FROM CUSTOMER WHERE isDeleted = 0", 
     (err, rows, fileds) => {
       res.send(rows);
     }
@@ -34,7 +35,7 @@ app.get('/api/customers', (req,res) => {
 app.use('./image', express.static('./upload'));
 
 app.post('./api/customers', upload.single('image'), (req, res) => {
-  let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+  let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, now(), 0)';
   let image = '/image/' + req.file.filename;
   let name = req.body.name;
   let name = req.body.birthday;
@@ -48,5 +49,14 @@ app.post('./api/customers', upload.single('image'), (req, res) => {
     }
     );
 });
+
+app.delete('./api/customer/:id', (req,res) => {
+  let sql = 'UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?';
+  let params = [req.param.id];
+  connection.query(sql, params,
+    (err, rows, fields) => {
+      res.send(rows);
+    })
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
