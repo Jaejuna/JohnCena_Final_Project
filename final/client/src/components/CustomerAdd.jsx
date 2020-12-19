3b6fc71422356b6b912284e062141b31ae52ca69
Jaejuna <anayana9988@gmail.com>
2020-12-19 21:00:15 +0900

    commiting

--- final/client/src/components/CustomerAdd.jsx
+++ final/client/src/components/CustomerAdd.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';   
 import { post } from 'axios';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
