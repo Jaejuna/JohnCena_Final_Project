dada0d23cfb8820ad0735d80bfbb0a5f88a7b6a8
Jaejuna <anayana9988@gmail.com>
2020-12-19 22:03:32 +0900

    search module added

--- final/client/src/index.js
+++ final/client/src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme ({
+  typography: {
+    fontFamily: '"Noto sans KR", serif',
+  }
+})
 
 ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
 
